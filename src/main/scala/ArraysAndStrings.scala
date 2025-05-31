51007d0345566aedb95392837cc76f5011710308
Makarand Patare <makarand.patare@hcl.com>
2025-05-31 08:18:10 +0530

    Arrays and Strings - 977. Squares for a Sorted Array

--- src/main/scala/ArraysAndStrings.scala
+++ src/main/scala/ArraysAndStrings.scala
@@ -1,19 +1,6 @@
 import scala.annotation.tailrec
 
 object ArraysAndStrings {
-  // https://leetcode.com/problems/reverse-string/description/
-  def reverseString(array: Array[Char]): Array[Char] = {
-    @tailrec
-    def loop(left: Int, right: Int): Array[Char] = {
-      if (left < right) {
-        val temp = array(left)
-        array(left) = array(right)
-        array(right) = temp
-        loop(left + 1, right - 1)
-      } else array
-    }
-    loop(0, array.length - 1)
-  }
   // https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
   def twoSum(numbers: Array[Int], target: Int): Array[Int] = {
     @tailrec
@@ -36,5 +23,38 @@ object ArraysAndStrings {
       else loop(i, j + 1)
     }
     loop(0, 0)
+  }
+  // https://leetcode.com/problems/reverse-string/description/
+  def reverseString(array: Array[Char]): Array[Char] = {
+    @tailrec
+    def loop(left: Int, right: Int): Array[Char] = {
+      if (left < right) {
+        val temp = array(left)
+        array(left) = array(right)
+        array(right) = temp
+        loop(left + 1, right - 1)
+      } else array
+    }
+    loop(0, array.length - 1)
+  }
+
+  // https://leetcode.com/problems/squares-of-a-sorted-array/description/
+  def sortedSquares(nums: Array[Int]): Array[Int] = {
+    val n = nums.length
+    var left = 0
+    var right = n - 1
+    val result = new Array[Int](n)
+    for (i <- n - 1 to 0 by -1) {
+      var largeNum = 0
+      if (math.abs(nums(right)) > math.abs(nums(left))) {
+        largeNum = nums(right)
+        right -= 1
+      } else {
+        largeNum = nums(left)
+        left += 1
+      }
+      result(i) = largeNum * largeNum
+    }
+    result
   }
 }
--- src/test/scala/ArraysAndStringsSpec.scala
+++ src/test/scala/ArraysAndStringsSpec.scala
@@ -1,31 +1,8 @@
-import ArraysAndStrings.{isSubsequence, reverseString, twoSum}
+import ArraysAndStrings.{isSubsequence, reverseString, sortedSquares, twoSum}
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers.shouldEqual
 
 class ArraysAndStringsSpec extends AnyFunSpec {
-  describe("reverseString") {
-    it("should reverse normal string") {
-      val inputString = Array[Char]('h','e','l','l','o')
-      val expectedString: Array[Char] = Array[Char]('o', 'l', 'l', 'e', 'h')
-      val result: Array[Char] = reverseString(inputString)
-      assert(result sameElements expectedString)
-    }
-    it("should reverse palindrome string") {
-      val inputString = Array[Char]('h', 'a', 'n', 'n', 'a', 'h')
-      val expectedString = Array[Char]('h', 'a', 'n', 'n', 'a', 'h')
-      assert(reverseString(inputString) sameElements expectedString)
-    }
-    it("should reverse palindrome with mixed case") {
-      val inputString = Array[Char]('H', 'A', 'n', 'N', 'a', 'h')
-      val expectedString = Array[Char]('h', 'a', 'N', 'n', 'A', 'H')
-      assert(reverseString(inputString) sameElements expectedString)
-    }
-    it("should do nothing for empty string") {
-      val inputString = Array[Char]()
-      val expectedString = Array[Char]()
-      assert(reverseString(inputString) sameElements expectedString)
-    }
-  }
   describe("twoSum") {
     it("should return correct output for case 1") {
       val inputArray = Array(2,7,11,15)
@@ -60,6 +37,43 @@ class ArraysAndStringsSpec extends AnyFunSpec {
       val expected = false
       val output = isSubsequence(inputS, inputT)
       assert(output == expected)
+    }
+  }
+  describe("reverseString") {
+    it("should reverse normal string") {
+      val inputString = Array[Char]('h', 'e', 'l', 'l', 'o')
+      val expectedString: Array[Char] = Array[Char]('o', 'l', 'l', 'e', 'h')
+      val result: Array[Char] = reverseString(inputString)
+      assert(result sameElements expectedString)
+    }
+    it("should reverse palindrome string") {
+      val inputString = Array[Char]('h', 'a', 'n', 'n', 'a', 'h')
+      val expectedString = Array[Char]('h', 'a', 'n', 'n', 'a', 'h')
+      assert(reverseString(inputString) sameElements expectedString)
+    }
+    it("should reverse palindrome with mixed case") {
+      val inputString = Array[Char]('H', 'A', 'n', 'N', 'a', 'h')
+      val expectedString = Array[Char]('h', 'a', 'N', 'n', 'A', 'H')
+      assert(reverseString(inputString) sameElements expectedString)
+    }
+    it("should do nothing for empty string") {
+      val inputString = Array[Char]()
+      val expectedString = Array[Char]()
+      assert(reverseString(inputString) sameElements expectedString)
+    }
+  }
+  describe("sortedSquares") {
+    it ("should return correct response for case 1") {
+      val input = Array(-4,-1,0,3,10)
+      val expected = Array(0, 1, 9, 16, 100)
+      val output = sortedSquares(input)
+      assert(output sameElements expected)
+    }
+    it("should return correct response for case 2") {
+      val input = Array(-7,-3,2,3,11)
+      val expected = Array(4, 9, 9, 49, 121)
+      val output = sortedSquares(input)
+      assert(output sameElements expected)
     }
   }
 }
